7b47cca41737bacf3d86ca2c408ece60eee76c75
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-07-18 18:54:14 -0300

    adiciona metodo para reenviar agendamento

--- src/pages/lista-agendamento/lista-agendamento.ts
+++ src/pages/lista-agendamento/lista-agendamento.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController, Alert, Loading } from 'ionic-angular';
 import { Agendamento } from '../../app/models/agendamento';
 import { AgendamentoDaoProvider } from '../../providers/agendamento-dao/agendamento-dao';
+import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
 
 @IonicPage()
 @Component({
@@ -15,15 +16,60 @@ export class ListaAgendamentoPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    private dao: AgendamentoDaoProvider) { }
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController,
+    private agendamentoService: AgendamentosServiceProvider,
+    private agendamentoDao: AgendamentoDaoProvider) { }
 
   ionViewDidLoad() {
-    this.dao.listaTodos().subscribe(
+    this.agendamentoDao.listaTodos().subscribe(
       (agendamentos: Agendamento[]) => {
         this.agendamentos = agendamentos;
       },
       (error) => { console.log(error); }
     );
+  }
+
+  reagendar(agendamento: Agendamento) {
+    let loading: Loading = this._loadingCtrl.create({
+      content: 'Carregando itens...'
+    });
+
+    loading.present();
+
+    let alert: Alert = this._alertCtrl.create({
+      buttons: [{text: 'Ok'}]
+    });
+
+    let titulo: string = '';
+    let mensagem: string = '';
+    
+    this.agendamentoService.agendar(agendamento)
+      .mergeMap((val) => {
+        let observable = this.agendamentoDao.salvar(agendamento);
+
+        if(val instanceof Error)
+          throw val;
+
+        return observable;
+      })
+      .finally(() => {
+        alert.setTitle(titulo);
+        alert.setSubTitle(mensagem);
+        alert.present();
+      })  
+      .subscribe(
+        () => {
+          loading.dismiss();
+          titulo = 'SUCESSO';
+          mensagem = 'Agendamento reenviado!';
+        }, 
+        (error) => {
+          console.log(error);
+          loading.dismiss();
+          titulo = 'ERRO';
+          mensagem = error.message;
+        });
   }
 
 }
