1a7ab5d7b50387e183adab3600523397c3a9a518
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-08-01 21:54:38 -0300

    inicia na pagina de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListaAgendamentoPage } from '../pages/lista-agendamento/lista-agendamento';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
+  selector: 'myapp',
   templateUrl: 'app.html'
 })
 export class MyApp {
   @ViewChild(Nav) public nav: Nav;
 
-  rootPage:any = HomePage;
+  rootPage:any = LoginPage;
 
   public paginas = [
     {titulo: 'Agendamentos', componente: ListaAgendamentoPage.name, icone: 'calendar'}
