5b8a34d6fd8cb1c07ad8520af6edbd36df7349d3
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-08-01 21:34:29 -0300

    array de paginas de menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { LoginPage } from '../pages/login/login';
+import { HomePage } from '../pages/home/home';
 import { ListaAgendamentoPage } from '../pages/lista-agendamento/lista-agendamento';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = ListaAgendamentoPage.name;
+  @ViewChild(Nav) public nav: Nav;
+
+  rootPage:any = HomePage;
+
+  public paginas = [
+    {titulo: 'Agendamentos', componente: ListaAgendamentoPage.name, icone: 'calendar'}
+  ];
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  irPara(pagina){
+    this.nav.push(pagina);
   }
 }
 
