d53140fd795a1bf6c6096aa99497d11680cea42e
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-08-15 20:29:26 -0300

    adiciona metodos para tirar foto, salvar e recuperar o avatar

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, normalizeURL } from 'ionic-angular';
 import { UsuarioServiceProvider } from '../../providers/usuario-service/usuario-service';
 import { Usuario } from '../../app/models/usuario';
+import { Camera } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -12,11 +13,31 @@ export class PerfilPage {
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
-    private usuarioService: UsuarioServiceProvider) {
+    private usuarioService: UsuarioServiceProvider,
+    private camera: Camera) {
+  }
+  
+  get avatar(): string {
+    return this.usuarioService.obtemAvatar();
   }
 
   get usuarioLogado(): Usuario {
     return this.usuarioService.obtemUsuarioLogado();
+  }
+  
+  tiraFoto(){
+    this.camera.getPicture({
+      destinationType: this.camera.DestinationType.FILE_URI,
+      saveToPhotoAlbum: true,
+      correctOrientation: true
+    })
+    .then(uri => {
+      uri = normalizeURL(uri);
+      this.usuarioService.salvaAvatar(uri);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
 
 }
